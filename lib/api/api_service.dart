39f77aa4cdbcf5e7cfa5a87041544fd874ca0918
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-10-31 23:13:54 +0800

    add API service endpoint login

---
+++ lib/api/api_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:flutterform/model/login_model.dart';
+import 'package:http/http.dart' as http;
+
+class APIService {
+  Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
+    String url = 'https://reqres.in/api/login';
+
+    final response =
+        await http.post(Uri.parse(url), body: requestModel.toJson());
+    if (response.statusCode == 200 || response.statusCode == 400) {
+      print(response.statusCode);
+      return LoginResponseModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load Data');
+    }
+  }
+}
